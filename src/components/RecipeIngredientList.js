1aa6a1fd8ce3a01adf01c048adc98dab5cb1696a
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-11-15 17:44:00 -0500

    radios for selecing grams or ounces for conversion

--- src/components/RecipeIngredientList.js
+++ src/components/RecipeIngredientList.js
@@ -1,36 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import RecipeIngredient from './RecipeIngredient'
-import { Segment, Table } from 'semantic-ui-react'
+import { Form, Radio, Button, Segment, Table } from 'semantic-ui-react'
 
-const RecipeIngredientList = ({ recipeIngredients }) => {
-  // console.log("RecipeIngredientList:", recipeIngredients)
+class RecipeIngredientList extends Component {
 
-  if (recipeIngredients.length > 0) {
+  state = {}
 
+  handleChange = (event, { value }) => this.setState({ value })
+
+  conversionRadios = () => {
     return (
-      <div style={{display: 'flex', margin:'5%', width:'90%', position: 'relative'}}>
-      <Segment style={{width:'100%'}}>
-      <Table>
-      <Table.Header>
-      <Table.Row style={{textAlign:'center'}}>
-      <Table.HeaderCell style={{width:'5%'}}></Table.HeaderCell>
-      <Table.HeaderCell style={{width:'5%'}}></Table.HeaderCell>
-      <Table.HeaderCell style={{width:'50%'}}>Ingredient</Table.HeaderCell>
-      <Table.HeaderCell style={{width:'20%'}}>Quantity</Table.HeaderCell>
-      <Table.HeaderCell style={{width:'20%'}}>Unit</Table.HeaderCell>
-      </Table.Row>
-      </Table.Header>
-      <Table.Body>
-      {recipeIngredients.map(ingredient => <RecipeIngredient ingredient={ingredient} key={ingredient.ingredient_name} />)}
-      </Table.Body>
-      </Table>
-      </Segment>
-      </div>
-
+      <Form>
+      <Form.Field>
+      <Radio label='Grams' value='grams' checked={this.state.value === 'grams'} onChange={this.handleChange} />
+      </Form.Field>
+      <Form.Field>
+      <Radio label='Ounces' value='ounces' checked={this.state.value === 'ounces'} onChange={this.handleChange} />
+      </Form.Field>
+      </Form>
     )
-  } else {
-    return null
+  }
+
+  render() {
+    console.log("RecipeIngredientList:", this.props, this.state)
+    if (this.props.recipeIngredients.length > 0) {
+
+      return (
+        <div style={{display: 'flex', margin:'5%', width:'90%', position: 'relative'}}>
+        <Segment style={{width:'100%'}}>
+        <Table>
+        <Table.Header>
+        <Table.Row style={{textAlign:'center'}}>
+        <Table.HeaderCell style={{width:'5%'}}></Table.HeaderCell>
+        <Table.HeaderCell style={{width:'5%'}}></Table.HeaderCell>
+        <Table.HeaderCell style={{width:'50%'}}>Ingredient</Table.HeaderCell>
+        <Table.HeaderCell style={{width:'20%'}}>Quantity</Table.HeaderCell>
+        <Table.HeaderCell style={{width:'20%'}}>Unit</Table.HeaderCell>
+        </Table.Row>
+        </Table.Header>
+        <Table.Body>
+        {this.props.recipeIngredients.map(ingredient => <RecipeIngredient ingredient={ingredient} key={ingredient.ingredient_name} />)}
+        </Table.Body>
+        </Table>
+        {this.conversionRadios()}
+        <Button type="submit">Convert Recipe</Button>
+        </Segment>
+        </div>
+
+      )
+    } else {
+      return null
+    }
   }
 }
 
