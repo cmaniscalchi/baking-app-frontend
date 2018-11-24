c6404255294e992039bbb08b8935be46b035e30c
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-11-24 15:40:52 -0500

    now displaying converted recipes

--- src/components/RecipeIngredient.js
+++ src/components/RecipeIngredient.js
@@ -1,20 +1,18 @@
+
 import React from 'react'
 import { connect } from 'react-redux'
 import { Table, Icon } from 'semantic-ui-react'
 import { openModal, removeIngredient, selectIngredient } from '../actions'
 
-const RecipeIngredient = ({ ingredient, openModal, recipeIngredients, removeIngredient, selectIngredient }) => {
+const RecipeIngredient = ({ conversionUnit, ingredient, openModal, recipeIngredients, removeIngredient, selectIngredient }) => {
 
   const handleSelect = ingredient => {
     selectIngredient(ingredient)
     openModal()
   }
 
-
-  console.log("RecipeIngredient:", ingredient, recipeIngredients, removeIngredient )
-  if (ingredient) {
+  const ingredientVolumes = () => {
     let { ingredient_name, ingredient_unit, ingredient_volume } = ingredient
-
     return (
       <Table.Row style={{textAlign: 'center'}}>
       <Table.Cell onClick={() => removeIngredient(ingredient)}><Icon name='delete' /></Table.Cell>
@@ -24,11 +22,51 @@ const RecipeIngredient = ({ ingredient, openModal, recipeIngredients, removeIngr
       <Table.Cell>{ingredient_volume.value > 1 ? ingredient_unit.name.plural : ingredient_unit.name.singular}</Table.Cell>
       </Table.Row>
     )
+  }
+
+  const ingredientGrams = () => {
+    let { ingredient_name, ingredient_unit, ingredient_grams } = ingredient
+    return (
+      <Table.Row style={{textAlign: 'center'}}>
+      <Table.Cell onClick={() => removeIngredient(ingredient)}><Icon name='delete' /></Table.Cell>
+      <Table.Cell onClick={() => handleSelect(ingredient)}><Icon name='edit' /></Table.Cell>
+      <Table.Cell>{ingredient_name}</Table.Cell>
+      <Table.Cell>{ingredient_grams}</Table.Cell>
+      <Table.Cell>{ingredient_grams > 1 ? "Grams" : "Gram"}</Table.Cell>
+      </Table.Row>
+    )
+  }
+
+  const ingredientOunces = () => {
+    let { ingredient_name, ingredient_unit, ingredient_ounces } = ingredient
+    return (
+      <Table.Row style={{textAlign: 'center'}}>
+      <Table.Cell onClick={() => removeIngredient(ingredient)}><Icon name='delete' /></Table.Cell>
+      <Table.Cell onClick={() => handleSelect(ingredient)}><Icon name='edit' /></Table.Cell>
+      <Table.Cell>{ingredient_name}</Table.Cell>
+      <Table.Cell>{Math.round(ingredient_ounces * 10)/10}</Table.Cell>
+      <Table.Cell>{ingredient_ounces > 1 ? "Ounces" : "Ounce"}</Table.Cell>
+      </Table.Row>
+    )
+  }
+
+
+  console.log("RecipeIngredient:", ingredient, recipeIngredients, removeIngredient )
+  if (ingredient) {
+    if (!conversionUnit) {
+      return ingredientVolumes()
+    } else if (conversionUnit === "ounces") {
+      return ingredientOunces()
+    } else if (conversionUnit === "grams") {
+      return ingredientGrams()
+    } else {
+      return null
+    }
   } else {
     return null
   }
 }
 
-const mapStateToProps = ({ recipes: { recipeIngredients } }) => ({ recipeIngredients })
+const mapStateToProps = ({ recipes: { conversionUnit, recipeIngredients } }) => ({ conversionUnit, recipeIngredients })
 
 export default connect(mapStateToProps, { openModal, removeIngredient, selectIngredient })(RecipeIngredient)
